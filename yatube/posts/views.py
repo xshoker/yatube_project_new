26bc56fc01f44dcfc432720da8ed8badf3d87245
xshoker <xshoker@yandex-team.ru>
2022-06-17 13:27:12 +0300

    Добавлены html

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
-# ice_cream/views.py
 from django.http import HttpResponse
 
 
 # Главная страница
-def index(request):    
-    return HttpResponse('Главная страница')
+# ice_cream/views.py
+from django.http import HttpResponse
+
+# Главная страница
+def index(request):
+    html_content = '<html><head><title>Yatube</title></head><body>'
+    html_content += '<h1>Главная страница</h1>'
+    html_content += '</body></html>'    
+    return HttpResponse(html_content)
 
 
 # Страница со списком мороженого
