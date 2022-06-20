2709b9d2ff001de2beee41a21bee6dd03e404ca4
xshoker <xshoker@yandex-team.ru>
2022-06-20 13:42:30 +0300

    Добавлены render group4

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 
 from posts import views
 
+app_name = 'posts'
+
 urlpatterns = [
     path('', views.index),
-    path('group/', views.group, name = 'posts_list'),
+    path('group/', views.group, name='posts_list'),
     path('group/<slug:pk>/', 
     views.group_posts),
 ]
